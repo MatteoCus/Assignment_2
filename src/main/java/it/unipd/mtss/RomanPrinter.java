a6b4cc0ef1305295752948fc5722610af8c34afc
Matteo Cusin <matteo.cusin58@gmail.com>
2023-05-16 11:59:46 +0200

    close #14

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -6,12 +6,78 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-        public static String print(int num) throws InvalidNumberException
-        {
-            return printAsciiArt(IntegerToRoman.convert(num));
+    public static String print(int num) throws InvalidLetterException, InvalidNumberException {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber) throws InvalidLetterException {
+        String result = "";
+        switch (romanNumber) {
+            case "I":
+                result +=   "  _____  \n" +  
+                            " |_   _| \n" + 
+                            "   | |   \n" +
+                            "   | |   \n" +
+                            "  _| |_  \n" + 
+                            " |_____| \n";
+                break;
+            case "V":
+                result +=   " __      __ \n" +
+                            " \\ \\    / / \n" +
+                            "  \\ \\  / /  \n" +
+                            "   \\ \\/ /   \n" +
+                            "    \\  /    \n" +
+                            "     \\/     \n";
+                break;
+
+            case "X":
+                result +=   " __   __ \n" +
+                            " \\ \\ / / \n" +
+                            "  \\ V /  \n" +
+                            "   > <   \n" +
+                            "  / . \\  \n" +
+                            " /_/ \\_\\ \n" ;
+                break;
+
+            case "L":
+                result +=   "  _       \n" +
+                            " | |      \n" +
+                            " | |      \n" +
+                            " | |      \n" +
+                            " | |____  \n" +
+                            " |______| \n" ;
+                break;
+
+            case "C":
+                result +=   "   _____  \n" +
+                            "  / ____| \n" +
+                            " | |      \n" +
+                            " | |      \n" +
+                            " | |____  \n" +
+                            "  \\_____| \n";
+                break;
+
+            case "D":
+                result +=   "  _____   \n" +
+                            " |  __ \\  \n" +
+                            " | |  | | \n" +
+                            " | |  | | \n" +
+                            " | |__| | \n" +
+                            " |_____/  \n";
+                break;
+
+                case "M":
+                result +=   "  __  __  \n" +
+                            " |  \\/  | \n" +
+                            " | \\  / | \n" +
+                            " | |\\/| | \n" +
+                            " | |  | | \n" +
+                            " |_|  |_| \n" ;
+                break;
+
+            default:
+                throw new InvalidLetterException();
         }
-    private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
+        return result;
     }
 }
