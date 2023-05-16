3224bc8d84dfc61c5eb0f7c576f24e4878f4d63f
checco296 <francescovillorba@gmail.com>
2023-05-16 12:12:03 +0200

    close #12

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -13,8 +13,8 @@ public class IntegerToRoman
         {
             throw new InvalidNumberException();
         }
-        final int[] val = {100,90,50,40,10,9,5,4,1};
-        final String[] rom = {"C","XC","L","XL","X","IX","V","IV","I"};
+        final int[] val = {500,400,100,90,50,40,10,9,5,4,1};
+        final String[] rom = {"D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
         StringBuilder ans = new StringBuilder();
         for (int i = 0; number > 0; i++)
