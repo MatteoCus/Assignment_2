4112cf4fb5a1dbfd4e30dac676c7ad5ec9be3f0a
checco296 <francescovillorba@gmail.com>
2023-05-16 12:14:28 +0200

    close #13

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -29,7 +29,8 @@ public class IntegerToRomanTest
         public static Collection input()
         {
             return Arrays.asList(new Object[][] {{1,"I"},{2,"II"},{3,"III"},{4,"IV"},{5,"V"},{6,"VI"},{7,"VII"},
-                {8,"VIII"},{9,"IX"},{10,"X"},{20,"XX"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"}});
+                {8,"VIII"},{9,"IX"},{10,"X"},{20,"XX"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},
+                {900,"CM"},{1000,"M"}});
         }
 
         @Test
